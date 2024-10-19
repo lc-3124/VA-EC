4320f4715942558aedd03dc85c844d66ff31af4d
Lc3124 <3418746552@qq.com>
2024-10-19 22:10:39 +0800

    将声明与实现文件用内联函数的方式放在了一一个文件里面，嗯，这个类库差不多就开发到这，而我也将要进行其他项目，毕竟这个项目本来就是附属品，之后有问题再回来push  The declaration and implementation files are placed in one file using inline functions. This library is developed almost to this point. I will also start other projects. After all, this project is originally an accessory. If there are any problems later, I will come back and push.

--- VaEcControl.hpp
+++ VaEcControl.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <cstdio>
 #include "ColorEnum.hpp"
 
 /*
  * This is a class that contains functions used to manage some Escape characters.
  * It is designed to provide useful functionality for terminal control.
- * So I like it , it could be used for long time  :)
+ * So I like it, it could be used for long time  :)
  */
 /*
  * Enum definitions for cursor movement directions and color codes.
@@ -21,88 +22,233 @@ enum
 
 class VaEcControl
 {
-private:
-    // No private members currently, but may be added in the future.
-public:
-    /*
-     * Functions related to screen actions.
-     */
-    // Clear the entire screen.
-    static const char* _clear();
-    static void         clear();
+    private:
+        // No private members currently, but may be added in the future.
+    public:
+        /*
+         * Functions related to screen actions.
+         */
+        // Clear the entire screen.
+        inline static const char* _clear()
+        {
+            return "\033[2J";
+        }
+        inline static void clear()
+        {
+            printf("\033[2J");
+        }
 
-    // Clear the area from the cursor's position to the end of the line.
-    static const char* _clear_line();
-    static void         clear_line();
+        // Clear the area from the cursor's position to the end of the line.
+        inline static const char* _clear_line()
+        {
+            return "\033[K";
+        }
+        inline static void clear_line()
+        {
+            printf("\033[K");
+        }
 
-    /*
-     * Functions related to cursor actions.
-     */
-    // Move the cursor to the specified position (h, w).
-    static const char* _cur_moveto(int h, int w);
-    static void         cur_moveto(int h, int w);
+        /*
+         * Functions related to cursor actions.
+         */
+        // Move the cursor to the specified position (h, w).
+        inline static const char* _cur_moveto(int h, int w)
+        {
+            static char escapeCommand[64];
+            snprintf(escapeCommand, sizeof(escapeCommand), "\033[%d;%dH", h, w);
+            return escapeCommand;
+        }
+        inline static void cur_moveto(int h, int w)
+        {
+            printf(_cur_moveto(h, w));
+        }
 
-    // Move the cursor in a given direction by a specified distance.
-    // e.g. cout << _move(LEFT, 10);
-    static const char* _cur_move(int dr, int ds);
-    static void         cur_move(int dr, int ds);
+        // Move the cursor in a given direction by a specified distance.
+        // e.g. cout << _move(LEFT, 10);
+        inline static const char* _cur_move(int dr, int ds)
+        {
+            static char escapeCommand[64];
+            switch (dr)
+            {
+                case CUR_LEFT:
+                    {
+                        snprintf(escapeCommand, sizeof(escapeCommand), "\033[%dD", ds);
+                        return escapeCommand;
+                    }
+                case CUR_RIGHT:
+                    {
+                        snprintf(escapeCommand, sizeof(escapeCommand), "\033[%dC", ds);
+                        return escapeCommand;
+                    }
+                case CUR_UP:
+                    {
+                        snprintf(escapeCommand, sizeof(escapeCommand), "\033[%dA", ds);
+                        return escapeCommand;
+                    }
+                case CUR_DOWN:
+                    {     snprintf(escapeCommand, sizeof(escapeCommand), "\033[%dB", ds);
+                        return escapeCommand;
+                    }default:
+                    return nullptr;
+            }
+        }
+        inline static void cur_move(int dr, int ds)
+        {
+            printf(_cur_move(dr, ds));
+        }
 
-    // Reset the cursor to its default position.
-    static const char* _cur_reset();
-    static void         cur_reset();
+        // Reset the cursor to its default position.
+        inline static const char* _cur_reset()
+        {
+            return "\033[H";
+        }
+        inline static void cur_reset()
+        {
+            printf("\033[H");
+        }
 
-    // Hide the cursor.
-    static const char* _cur_hide();
-    static void         cur_hide();
+        // Hide the cursor.
+        inline static const char* _cur_hide()
+        {
+            return "\033[?25l";
+        }
+        inline static void cur_hide()
+        {
+            printf("\033[?25l");
+        }
 
-    // Show the cursor.
-    static const char* _cur_show();
-    static void         cur_show();
+        // Show the cursor.
+        inline static const char* _cur_show()
+        {
+            return "\033[?25h";
+        }
+        inline static void cur_show()
+        {
+            printf("\033[?25h");
+        }
 
-    /*
-     * Functions related to color control.
-     */
-    // Set the text and background colors.
-    // Remember to reset before returning to system , you know why  
-    static const char* _set_color    (int front, int background);
-    static void         set_color    (int front, int background);
+        /*
+         * Functions related to color control.
+         */
+        // Set the text and background colors.
+        // Remember to reset before returning to system, you know why  
+        inline static const char* _set_color(int front, int background)
+        {
+            static char escapeCommand[64];
+            snprintf(escapeCommand, sizeof(escapeCommand), "\033[%dm\033[%dm", front, background);
+            return escapeCommand;
+        }
+        inline static void set_color(int front, int background)
+        {
+            printf(_set_color(front, background));
+        }
 
-    static const char* _set_color16  (int front, int background);
-    static void         set_color16  (int front, int background);
+        inline static const char* _set_color16(int front, int background)
+        {
 
-    static const char* _set_color256 (int front, int background);
-    static void         set_color256 (int front, int background);
-    
-    //My console is not supporting RGB ,so I don't know if it can work
-    static const char* _set_background_color_RGB(int R ,int B ,int G);
-    static void         set_background_color_RGB(int R, int B ,int G);
-    
-    static const char* _set_front_color_RGB(int R ,int B ,int G);
-    static void         set_front_color_RGB(int R, int B ,int G);
-    
-    /*
-     * Functions related to effect cotrol.
-     */
-    // Enable or disable highlighting.
-    static const char* _high_light();
-    static void         high_light(); 
-    static const char* _un_high_light();
-    static void         un_high_light();
+            static char escapeCommand[64];
+            snprintf(escapeCommand, sizeof(escapeCommand),"\033[38;5;%dm\033[48;5;%dm",front,background);
+            return escapeCommand;
+        }
+        inline static void set_color16(int front, int background)
+        {
+            printf(_set_color16(front, background));
+        }
 
-    // Underline or disable 
-    static const char*  _underline();
-    static void          underline();
-    static const char*  _un_underline();
-    static void          un_underline();
+        inline static const char* _set_color256(int front, int background)
+        { 
 
-    // Bright (This funcation is included in set_color16() ) or disable 
-    // It may occurs effect on the 16color setting
-    static const char*  _bright();
-    static void          bright();
-    
-    static const char*  _un_bright();
-    static void          un_bright();
+            static char escapecommand[64];
+            snprintf(escapecommand, sizeof(escapecommand),"\033[38;5;%dm\033[48;5;%dm",front,background);
+            return escapecommand;
+        }
+        inline static void set_color256(int front, int background)
+        {
+            printf(_set_color256(front, background));
+        }
 
+        //My console is not supporting RGB,so I don't know if it can work
+        inline static const char* _set_background_color_RGB(int R,int B,int G)
+        {
+
+            static char escapecommand[64];
+            snprintf(escapecommand, sizeof(escapecommand),"\033[48;2;%d,%d,%dm",R,G,B);
+            return escapecommand;
+        }
+        inline static void set_background_color_RGB(int R, int B,int G)
+        {
+            printf(_set_background_color_RGB(R, B, G));
+        }
+
+        inline static const char* _set_front_color_RGB(int R,int B,int G)
+        {
+
+            static char escapecommand[64];
+            snprintf(escapecommand, sizeof(escapecommand),"\033[38;2;%d,%d,%dm",R,G,B);
+            return escapecommand;
+        }
+        inline static void set_front_color_RGB(int R, int B,int G)
+        {
+            printf(_set_front_color_RGB(R, G, B));
+        }
+
+        /*
+         * Functions related to effect cotrol.
+         */
+        // Enable or disable highlighting.
+        inline static const char* _high_light()
+        {
+            return "\033[7m";
+        }
+        inline static void high_light()
+        {
+            printf("\033[7m");
+        } 
+        inline static const char* _un_high_light()
+        {
+            return "\033[27m";
+        }
+        inline static void un_high_light()
+        {
+            printf("\033[27m");
+        }
+
+        // Underline or disable 
+        inline static const char*  _underline()
+        {
+            return "\033[4m";
+        }
+        inline static void underline()
+        {
+            printf("\033[4m");
+        }
+        inline static const char*  _un_underline()
+        {
+            return "\033[24m";
+        }
+        inline static void un_underline()
+        {
+            printf("\033[24m");
+        }
+
+        // It may occurs effect on the 16color setting
+        inline static const char*  _bright()
+        {
+            return "\033[1m";
+        }
+        inline static void bright()
+        {
+            printf("\033[1m");
+        }
+
+        inline static const char*  _un_bright()
+        {
+            return "\033[21m";
+        }
+        inline static void un_bright()
+        {
+            printf("\033[21m");
+        }
 };
 
 typedef VaEcControl vaec;
